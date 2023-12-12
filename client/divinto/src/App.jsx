5e3afdc87368e21d2ba911dae6597d3ed0dd589e
7Jiun <jjackywu8888tw@gmail.com>
2023-12-12 16:14:12 +0800

    setup-s3-static-website

--- client/divinto/src/App.jsx
+++ client/divinto/src/App.jsx
@@ -16,7 +16,7 @@ import './text-updater-note.css';
 import './updatenode.css';
 import './App.css';
 
-export const URL = '';
+export const URL = 'http://18.177.185.171';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
